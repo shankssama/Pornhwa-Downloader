04c57acefde4d819192df86d6d9e31a9985d2a47
Sʜᴀɴᴋs <98250680+shankssama@users.noreply.github.com>
2022-07-23 14:21:50 +0545

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio as aio
+import os
+
+from bot import bot, manga_updater
+from models import DB
+
+
+async def async_main():
+    db = DB()
+    await db.connect()
+
+if __name__ == '__main__':
+    loop = aio.get_event_loop_policy().get_event_loop()
+    loop.run_until_complete(async_main())
+    loop.create_task(manga_updater())
+    bot.run()
