c15cffbb8f0f5323a2b07a6058635b3c125bd962
Sʜᴀɴᴋs <98250680+shankssama@users.noreply.github.com>
2022-07-23 13:37:52 +0545

    Create __init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,14 @@
+from .client import MangaClient, MangaCard, MangaChapter
+from .manhuako import ManhuaKoClient
+from .manhuaplus import ManhuaPlusClient
+from .tmo import TMOClient
+from .mangadex import MangaDexClient
+from .mangasee import MangaSeeClient
+from .mangasin import MangasInClient
+from .mcreader import McReaderClient
+from .mangakakalot import MangaKakalotClient
+from .manganelo import ManganeloClient
+from .manganato import ManganatoClient
+from .kissmanga import KissMangaClient
+from .mangatigre import MangatigreClient
+from .mangahasu import MangaHasuClient
